b556e79962574509fbd045398a62ba04ce445c55
John Shrader <figurefactory@gmail.com>
2021-05-24 14:51:42 -0400

    Update helperFunctions.js

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,6 +1,6 @@
 /**
  * date comes in format "2012-01-01T08:00:00Z" (year-month-day-Time)
- * split on "T or , or space"
+ * split on "T or - or space"
  * convert to month-day-year and return
  */
 
